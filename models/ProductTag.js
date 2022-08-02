82e42fbd299cb7745c08ac9595e93e2dbb4f78ba
jaysmith13 <janeasmith13@gmail.com>
2022-08-02 01:07:03 -0600

    folder corrections

---
+++ models/ProductTag.js
@@ -0,0 +1,20 @@
+const { Model, DataTypes } = require('sequelize');
+
+const sequelize = require('../Develop/config/connection');
+
+class ProductTag extends Model {}
+
+ProductTag.init(
+  {
+    // define columns
+  },
+  {
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'product_tag',
+  }
+);
+
+module.exports = ProductTag;
